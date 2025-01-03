c30c7de43153afd50a4a64cab5d5c93be394e089
dan <md.erhenede@gmail.com>
2025-01-03 19:07:05 +0100

    server update 2

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,28 +14,35 @@ const app = express();
 
 const PORT = process.env.PORT || 8800;
 
-const mongoUrl = process?.env?.MONGO_URL
+const mongoUrl = process?.env?.MONGO_URL;
 
-if (!mongoUrl) { throw new Error("MONGO_URL environment variable is not defined"); }
+if (!mongoUrl) {
+  throw new Error("MONGO_URL environment variable is not defined");
+}
 
 // connect to mongoDB
 mongoose.connect(mongoUrl);
 
-mongoose.connection.on("connected", () => { console.log("Connected to MongoDB"); });
-mongoose.connection.on("error", (err) => { console.error("MongoDB connection error:", err); });
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+mongoose.connection.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
+});
 
 // middleware
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static('public', {
-  setHeaders: (res, path, stat) => {
-      if (path.endsWith('.js')) {
-          res.setHeader('Content-Type', 'application/javascript');
+app.use(
+  express.static("public", {
+    setHeaders: (res, path, stat) => {
+      if (path.endsWith(".js")) {
+        res.setHeader("Content-Type", "application/javascript");
       }
-  }
-}));
-
+    },
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("hello");
@@ -50,3 +57,5 @@ app.use("/api/messages", messagesRoute);
 app.listen(PORT, () => {
   console.log(`server running http://localhost:${PORT}`);
 });
+
+export default app;
