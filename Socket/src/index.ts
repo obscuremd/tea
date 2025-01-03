bcfa7b958d79b1e1e4987d66ef9734843f8a418d
dan <md.erhenede@gmail.com>
2025-01-03 20:27:03 +0100

    server modification

--- Socket/src/index.ts
+++ Socket/src/index.ts
@@ -1,66 +1,70 @@
-import {Server} from 'socket.io'
-import express from 'express';
-import http from 'http';
+import { Server } from "socket.io";
+import express from "express";
+import http from "http";
 
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
 
-const port = 8800
+const port = 8800;
 
-const io = new Server(server,{
-    cors:{
-        // origin: 'https://pinoy-eight.vercel.app',
-        origin: 'http://localhost:5173',
-        methods: ['GET','POST','HEAD','DELETE','UPDATE']
-    }
-})
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://tea-dusky.vercel.app",
+    ],
+    methods: ["GET", "POST", "HEAD", "DELETE", "UPDATE"],
+  },
+});
 
 let users: Array<{ username: string; socketId: string }> = [];
 
 const addUser = (username: string, socketId: string) => {
   // Check if the user already exists in the users array
-  !users.some((user) => user.username === username) && users.push({ username, socketId });
+  !users.some((user) => user.username === username) &&
+    users.push({ username, socketId });
 };
 
-const removeUser = (socketId:string)=>{
-    users.filter(user => user.socketId !== socketId)
-}
+const removeUser = (socketId: string) => {
+  users.filter((user) => user.socketId !== socketId);
+};
 
-const getUser = (username:string) =>{
-    return users.find(user => user.username === username)
-}
+const getUser = (username: string) => {
+  return users.find((user) => user.username === username);
+};
 
-io.on('connection',(socket)=>{
-    // when connected
-    console.log('user connected', socket.id)
-    
-    // get user id and socket id
-    socket.on("addUser",username =>{
-        addUser(username, socket.id)
-        io.emit('getUsers',users)
-    })
+io.on("connection", (socket) => {
+  // when connected
+  console.log("user connected", socket.id);
 
-    // send message
-    socket.on("sendMessage",({senderName,receiverName,text})=>{
-        const user = getUser(senderName)
-        if(!user){
-            return
-        }else{
-            io.to(user?.socketId).emit("getMessage",{
-                senderName,text
-            })
-        }
-    })
+  // get user id and socket id
+  socket.on("addUser", (username) => {
+    addUser(username, socket.id);
+    io.emit("getUsers", users);
+  });
 
-    // when disconnect
-    socket.on("disconnect",()=>{
-        console.log('a user disconnected')
-        removeUser(socket.id)
-        io.emit('getUsers',users)
-    })
+  // send message
+  socket.on("sendMessage", ({ senderName, receiverName, text }) => {
+    const user = getUser(senderName);
+    if (!user) {
+      return;
+    } else {
+      io.to(user?.socketId).emit("getMessage", {
+        senderName,
+        text,
+      });
+    }
+  });
 
-})
+  // when disconnect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
 
-server.listen(port,()=>{
-    console.log(`listening on port https://localhost:${port}`)
-})
+server.listen(port, () => {
+  console.log(`listening on port https://localhost:${port}`);
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -32,7 +32,27 @@ mongoose.connection.on("error", (err) => {
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (
+        !origin ||
+        [
+          "http://localhost:5173",
+          "http://localhost:5174",
+          "https://tea-dusky.vercel.app",
+        ].includes(origin)
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, // Allow cookies and credentials
+  })
+);
 
 app.use(
   express.static("public", {
